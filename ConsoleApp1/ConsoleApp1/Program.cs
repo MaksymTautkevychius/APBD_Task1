c5b1178ab2211ab8ed45cfeba4e3496dade34c48
MaksymTautkevychius <maksymtautkevychius@gmail.com>
2024-03-14 22:58:16 +0100

    new feature

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,19 @@
 ﻿
 
-Console.WriteLine("Hello, Google");
+namespace Main
+{
+    class Program
+    {
+         static int Avg(int[] vals)
+         {
+             int sum=0;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                sum += vals[i];
+            }
+
+            int avg = sum / vals.Length;
+            return avg;
+         }
+    }
+}
